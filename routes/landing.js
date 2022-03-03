aa7a93629899a8a9c46adafea9340c33f117b502
dcabangon <richardnelsoncabangon@gmail.com>
2022-03-03 10:39:19 +0000

    added about and contact routes

--- routes/landing.js
+++ routes/landing.js
@@ -5,4 +5,12 @@ router.get('/', (req,res)=>{
     res.render('landing/index')
 })
 
+router.get('/about-us', (req,res)=>{
+    res.render('landing/about-us')
+})
+
+router.get('/contact-us', (req,res)=>{
+    res.render('landing/contact-us')
+})
+
 module.exports = router;
