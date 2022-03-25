082042e6dccbe841068aa75e0adf1a59b3ef9981
dcabangon <richardnelsoncabangon@gmail.com>
2022-03-25 13:00:57 +0000

    integrated caolan form

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const { createProductForm, bootstrapField } = require("../forms");
 const router = express.Router();
 
 // #1 import in the Product model
 const {Product} = require('../models')
+
+// Import in createProductForm and bootstrapField
+const {bootstrapField, createProductForm} = require ('../forms')
+
 
 router.get('/', async (req,res)=>{
     // #2 - fetch all the products (ie, SELECT * from products)
@@ -11,6 +16,10 @@ router.get('/', async (req,res)=>{
     res.render('products/index', {
         'products': products.toJSON() // #3 - convert collection to JSON
     })
+})
+
+router.get('/create' , function(req,res){
+    res.send("Creating a new product");
 })
 
 module.exports = router;
